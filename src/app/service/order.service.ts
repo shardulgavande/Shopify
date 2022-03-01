338f1fd9488021154539f37fe9bc5604354377e3
Shardul Gavande <shardulgavande@gmail.com>
2022-03-01 13:19:41 +0530

    Payment and Order Items
    Payment and Order Items

--- src/app/service/order.service.ts
+++ src/app/service/order.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class OrderService {
-  private base_url = "http://localhost:3000";
+  private base_url = "http://localhost:53176";
   http_option = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
@@ -17,13 +17,13 @@ export class OrderService {
   }
 
   create(data:any):Observable<any>{
-    return this.httpClient.post(this.base_url+"/app/orders/create",data,this.http_option);
+    return this.httpClient.post(this.base_url+"/api/orders",data,this.http_option);
   }
 
   createItem(data:any):Observable<any>{
-    return this.httpClient.post(this.base_url+"/app/orders/items/add",data,this.http_option);
+    return this.httpClient.post(this.base_url+"/api/orderitem",data,this.http_option);
   }
   createPayment(data:any):Observable<any>{
-    return this.httpClient.post(this.base_url+"/app/payment/add",data,this.http_option);
+    return this.httpClient.post(this.base_url+"/api/payment",data,this.http_option);
   }
 }
